70d99c0c8684046e20cc0643c16fba96d238496f
Daniel Tobon <danieltobonco@gmail.com>
2022-07-22 10:35:03 -0500

    added ej_2 pkg to sys path

--- ej_2/main.py
+++ ej_2/main.py
@@ -1,31 +1,39 @@
 import json
 import pickle
+import sys
+import os
 
+sys.path.append(os.path.join(os.getcwd(), ".."))
 from ej_2.services.tuler import Tuler
+from ej_2.services.ferretero import Ferretero
 from ej_2.enums.risk_enum import RiskEnum
 
-
-input_data = json.load(open('data/input_data.json'))
-with open('data/output_data.pickle', 'rb') as file:
+with open(os.path.join("data", "input_data.json")) as f:
+    input_data = json.load(f)
+with open(os.path.join("data", "output_data.pickle"), "rb") as file:
     expected_output = pickle.load(file)
 
 
 def choose_model(input: dict):
     obj = None
-    if input['product_name'] == RiskEnum('ferretero').value:
-        obj = Ferretero(user_id=input['user_id'],
-                        product_name=input['product_name'],
-                        input_data=input['input_data'])
-    elif input['product_name'] == RiskEnum('tuler').value:
-        obj = Tuler(user_id=input['user_id'],
-                    product_name=input['product_name'],
-                    input_data=input['input_data'])
+    if input["product_name"] == RiskEnum("ferretero").value:
+        obj = Ferretero(
+            user_id=input["user_id"],
+            product_name=input["product_name"],
+            input_data=input["input_data"],
+        )
+    elif input["product_name"] == RiskEnum("tuler").value:
+        obj = Tuler(
+            user_id=input["user_id"],
+            product_name=input["product_name"],
+            input_data=input["input_data"],
+        )
     return obj
 
 
 def check_result(output_data):
     assert output_data == expected_output
-    print('Buen trabajo')
+    print("Buen trabajo")
 
 
 def main():
@@ -36,5 +44,5 @@ def main():
     check_result(output_data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
